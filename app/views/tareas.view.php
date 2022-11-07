b9b212095768639627eca823fa7f4ecb417f702c
Matias Perez Viecho <mpviecho@gmail.com>
2022-11-06 23:10:07 -0300

    asd

--- app/views/tareas.view.php
+++ app/views/tareas.view.php
@@ -1,6 +1,12 @@
 <?php
+require_once './appTareas/smarty/libs/Smarty.class.php';
+
 class tareasView{
+    private $smarty;
     
+    function __construct(){
+        $this->smarty = new Smarty();
+    }
     function showHome($tareas){
         foreach ($tareas as $elem) {
             echo "<ul>
@@ -10,5 +16,8 @@ class tareasView{
                     <li><a href='info/{$elem->id_tarea}'>+ info</a></li>
                 </ul>";
         }
+
+        $this->smarty->assign("prueba", "Valor");
     }
+
 }
